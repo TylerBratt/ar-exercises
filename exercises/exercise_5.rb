3af06d37a1f009b46273b9ff8f5cf6243044199c
Tyler Bratt <tyler~bratt@hotmail.com>
2021-05-24 18:10:48 -0400

    update exercise_5, _6

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts total_revenue = Store.sum(:annual_revenue)
+puts average_revenue = Store.average(:annual_revenue)
+puts million_dollar_babies = Store.where("annual_revenue >=?",1000000).count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,23 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "LeVar", last_name: "Burton", hourly_rate: 160)
+
+@store1.employees.create(
+  first_name: "Frank", last_name: "Dux",
+  hourly_rate: 45)
+
+@store1.employees.create(
+  first_name: "JoeyJoeJoe", last_name: "Shabadoo", hourly_rate: 15)
+
+@store2.employees.create(
+  first_name: "Estelle", last_name: "Costanza", hourly_rate: 15)
+
+@store2.employees.create(
+  first_name: "Rosa", 
+  last_name: "Diaz", hourly_rate: 30)
+
+@store2.employees.create(
+  first_name: "Vito", 
+  last_name: "Andolini", hourly_rate: 1060)
